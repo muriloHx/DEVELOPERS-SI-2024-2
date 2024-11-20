018b312e1ef8c8790d31fd7623beb8f2b59dee74
Murilo <twitterfo131@gmail.com>
2024-11-20 17:05:58 -0300

    Imagens das paginas de servico

--- scripts/servicoJS.js
+++ scripts/servicoJS.js
@@ -8,33 +8,50 @@ document.addEventListener("DOMContentLoaded", () => {
 //objetos com as imagens do card de cada pagina
 const casamentoImgs = [
     {
-        imagem: '../imagens/bkgnd/bkg2.jpg',
-        href: '#'
+        imagem: '../imagens/servicos/cas1.jpg',
+        href: 'https://www.instagram.com/p/DBfB_FzsBCi/?img_index=6'
     },
     {
-        imagem: '../imagens/bkgnd/bkg3.jpg',
-        href: '#'
+        imagem: '../imagens/servicos/cas2.jpg',
+        href: 'https://www.instagram.com/p/DBfB_FzsBCi/?img_index=6'
+
+    },
+    {
+        imagem: '../imagens/servicos/cas3.jpg',
+        href: 'https://www.instagram.com/p/DBfB_FzsBCi/?img_index=6'
 
     }
 ]
 const aniversarioImgs = [
     {
-        imagem: '../imagens/bkgnd/bkg1.jpg',
-        href: '#'
+        imagem: '../imagens/servicos/ani1.jpg',
+        href: 'https://www.instagram.com/p/C9AlvuZgbH4/?img_index=1'
     },
     {
-        imagem: '../imagens/bkgnd/bkg3.jpg',
-        href: '#'
+        imagem: '../imagens/servicos/ani2.jpg',
+        href: 'https://www.instagram.com/p/C6yfJOHs5do/?img_index=3'
+
+    },
+    {
+        imagem: '../imagens/servicos/ani3.jpg',
+        href: 'https://www.instagram.com/p/C-IEQqcR435/?img_index=1'
+
     }
 ]
 const corporativoImgs = [
     {
-        imagem: '../imagens/bkgnd/bkg5.jpg',
-        href: '#'
+        imagem: '../imagens/servicos/cor1.jpg',
+        href: 'https://www.instagram.com/p/DADscwXsOWL/?img_index=5'
     },
     {
-        imagem: '../imagens/bkgnd/bkg3.jpg',
-        href: '#'
+        imagem: '../imagens/servicos/cor2.jpg',
+        href: 'https://www.instagram.com/p/C_bgGXnpTs6/?img_index=3'
+
+    },
+    {
+        imagem: '../imagens/servicos/cor3.jpg',
+        href: 'https://www.instagram.com/p/C_bgGXnpTs6/?img_index=4'
+
     }
 ]
 let cardElemento, imagens, cardAtual = 0;
